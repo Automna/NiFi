c2a31e4034fac372348ae74d079f89173d92fc06
Colin Bowdery <cbowdery@automna.com>
2017-03-02 00:41:35 -0600

    Automna Processors

--- nifi-rel-nifi-1.1.2/nifi-nar-bundles/nifi-standard-bundle/nifi-standard-processors/src/main/java/org/apache/nifi/processors/automna/Parser.java
+++ nifi-rel-nifi-1.1.2/nifi-nar-bundles/nifi-standard-bundle/nifi-standard-processors/src/main/java/org/apache/nifi/processors/automna/Parser.java
@@ -188,26 +188,29 @@ public class Parser extends AbstractProcessor {
         //.evaluateAttributeExpressions(original)
         //.getValue();
         
+        final String xsltVendor = context.getProperty(VENDOR)
+                .evaluateAttributeExpressions(original)
+                .getValue();
+        final String xsltFileType = context.getProperty(FORMAT)
+                    .evaluateAttributeExpressions(original)
+                    .getValue();
+                
         try {
             FlowFile transformed = session.write(original, new StreamCallback() {
                 @Override
                 public void process(final InputStream rawIn, final OutputStream out) throws IOException {
                     try (final InputStream in = new BufferedInputStream(rawIn)) {
                     	                        
-                        final String xsltVendor = context.getProperty(VENDOR)
-                                .evaluateAttributeExpressions(original)
-                                .getValue();
-                            final String xsltFileType = context.getProperty(FORMAT)
-                                    .evaluateAttributeExpressions(original)
-                                    .getValue();
-                            
+                    	logger.info(xsltVendor + xsltFileType);
                         if (xsltVendor == "ERICSSON" && xsltFileType == "XML")
                         {
                         	final String xsltFileName = "Automna/ericsson_xml.xslt";
+                        	logger.info(xsltFileName);
                         } 
                         else if (xsltVendor == "NOKIA" && xsltFileType == "XML")
                     	{
                         	final String xsltFileName = "Automna/nokia_xml.xslt";
+                        	logger.info(xsltFileName);
                     	}
                         final Templates templates;
                         if (cache != null) {
